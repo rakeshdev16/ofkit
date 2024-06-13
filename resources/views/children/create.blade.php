39521967659bf52d22742d4e07e89ed168dcc7fd
Rohit Panchal <rohit.panchal@vinnisoft.org>
2024-06-13 17:07:50 +0530

    Remove livewire

--- resources/views/children/create.blade.php
+++ resources/views/children/create.blade.php
@@ -7,7 +7,117 @@
 
                 @include('components.bread-crumb', ['title' => 'Children', 'subTitle' => 'Add Children'])
 
-                @livewire('children-form')
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row mb-2">
+                                    <div class="col-lg-12">
+                                        <div class="mt-4 mt-xl-0">
+                                            <form wire:submit.prevent="save">
+                                                <div class="row">                                                            
+                                                    <div class="col-md-6">
+                                                        <div class="mb-3">
+                                                            <label class="form-label" for="formrow-firstname-input">First Name</label>
+                                                            <input type="text" class="form-control" placeholder="Enter First Name" wire:model="name">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="mb-3">
+                                                            <label class="form-label" for="formrow-firstname-input">Family Name</label>
+                                                            <input type="text" class="form-control" placeholder="Enter Family Name" wire:model="family_name">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="mb-3">
+                                                            <label class="form-label" for="formrow-email-input">.I.D</label>
+                                                            <input type="text" class="form-control" placeholder="Enter .I.D" wire:model="child_id">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="mb-3">
+                                                            <label class="form-label" for="formrow-email-input">Gender</label>
+                                                            <select class="form-select" wire:model="gender">
+                                                                <option>Select</option>
+                                                                <option>Male</option>
+                                                                <option>Female</option>
+                                                            </select>                                                        
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="mb-3">
+                                                            <label class="form-label" for="formrow-password-input">Date of Birth</label>
+                                                            <input type="date" class="form-control" placeholder="Enter date of birth" wire:model="dob">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="mb-3">
+                                                            <label class="form-label" for="formrow-password-input">Age</label>
+                                                            <input type="text" class="form-control" placeholder="Enter age" wire:model="age">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-12">
+                                                        <div class="mb-3">
+                                                            <label class="form-label" for="formrow-password-input">Address</label>
+                                                            <input type="text" class="form-control" placeholder="Enter address" wire:model="address">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="mb-3">
+                                                            <label class="form-label" for="formrow-password-input">Kindergarten</label>
+                                                            <input type="file" class="form-control" wire:model="kindergarten">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="mb-3">
+                                                            <label class="form-label" for="formrow-email-input">Functioning</label>
+                                                            <select class="form-select">
+                                                                <option>Select</option>
+                                                                <option>High</option>
+                                                                <option>Others</option>
+                                                            </select>                                                        
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="mb-3">
+                                                            <label class="form-label" for="formrow-password-input">Diagnosis</label>
+                                                            <input type="file" class="form-control" id="formrow-password-input">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="mb-3">
+                                                            <label class="form-label" for="formrow-email-input">Status</label>
+                                                            <select class="form-select">
+                                                                <option>Select</option>
+                                                                <option>New</option>
+                                                                <option>Continuing</option>
+                                                            </select>                                                        
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="mb-3">
+                                                            <label class="form-label" for="formrow-password-input">Tabam Services star</label>
+                                                            <input type="date" class="form-control" placeholder="Enter age" id="formrow-password-input">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="mb-3">
+                                                            <label class="form-label" for="formrow-password-input">HMO</label>
+                                                            <input type="file" class="form-control" id="formrow-password-input">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="mt-4">
+                                                    <button type="submit" class="btn btn-primary w-md">Submit</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         @include('layout.footer')
