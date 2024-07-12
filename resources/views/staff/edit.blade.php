171e5b38de89d8e7dec92abf5a9ddbb1601e3e4e
Rohit Panchal <rohit.panchal@vinnisoft.org>
2024-07-12 16:23:53 +0530

    Select 2

--- resources/views/staff/edit.blade.php
+++ resources/views/staff/edit.blade.php
@@ -310,6 +310,9 @@
         <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
         @include('staff.script')
         <script>
+            $(document).ready(function() {
+                $('.kindergarten').select2();
+            });
             $(document).on('click', '#previewStaffImage', function() {
                 $('#staffProfileInp').click();
             });
