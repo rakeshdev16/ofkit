4aa631e91da1741c71cb574b98c566e5be32f5b1
Rohit Panchal <rohit.panchal@vinnisoft.org>
2024-07-18 16:00:51 +0530

    Label

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -8,7 +8,7 @@
     <div class="page-wrapper">
         <div class="page-content">
             <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
-                <div class="breadcrumb-title pe-3">{{ __('staff.addBtnText') }}</div>
+                <div class="breadcrumb-title pe-3">Profile</div>
                 <div class="ps-3">
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb mb-0 p-0">
