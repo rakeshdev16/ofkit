4f916ce645def0202c164cc7eb1cbd635e8fe5a3
Rohit Panchal <rohit.panchal@vinnisoft.org>
2024-07-29 10:39:34 +0530

    Fixed age issue

--- resources/views/children/edit.blade.php
+++ resources/views/children/edit.blade.php
@@ -464,7 +464,8 @@
                 start.add(years, 'years');
                 var months = end.diff(start, 'months');
                 start.add(months, 'months');
-                var age = 'Years ' + years + '.' + months;
+                // var age = 'Years ' + years + '.' + months;
+                var age = years;
                 $('.age').val(age);
             });
 
