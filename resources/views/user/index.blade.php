04c77b10cdd8d619ded43bf390c88a46289b72cf
Rohit Panchal <rohit.panchal@vinnisoft.org>
2024-08-06 17:12:41 +0530

    remove previous route from profile

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -23,7 +23,7 @@
                 </div>
                 <div class="ms-auto">
                     <div class="">
-                        <a href="{!! URL::previous() !!}" class="btn button">{{ __('staff.back') }}</a>
+                        <a href="{{ route('dashboard') }}" class="btn button">{{ __('staff.back') }}</a>
                     </div>
                 </div>
             </div>
