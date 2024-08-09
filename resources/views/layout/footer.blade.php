d2d078735d6d8b54b243aaf755a8774317a2ed35
Rohit Panchal <rohit.panchal@vinnisoft.org>
2024-08-09 16:25:38 +0530

    Dynamic footer date

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="overlay toggle-icon"></div>
 <a href="#" class="back-to-top"><i class='bx bxs-up-arrow-alt'></i></a>
 <footer class="page-footer">
-    <p class="mb-0">Copyright © 2023. All right reserved.</p>
+    <p class="mb-0">Copyright © {{ date('Y') }}. All right reserved.</p>
 </footer>
