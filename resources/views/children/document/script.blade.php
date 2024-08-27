641adeb7991e19086dabecea732944d2183e074e
Rohit Panchal <rohit.panchal@vinnisoft.org>
2024-08-27 15:13:53 +0530

    Show hide fields null

--- resources/views/children/document/script.blade.php
+++ resources/views/children/document/script.blade.php
@@ -46,9 +46,11 @@
     $(document).on('change', '.occured', function() {
         var value = $(this).val();
         if (value == 0) {
+            $('.occuredReason').find('input, select, textarea').attr('value', '');
             $('.occuredDescription').hide();
             $('.occuredReason').show();
         } else {
+            $('.occuredDescription').find('input, select, textarea').attr('value', '');
             $('.occuredReason').hide();
             $('.occuredDescription').show();
         }
