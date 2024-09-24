1e9bcb5a1f3187c0fd776c2fb154faad2fd49d34
ankitdhiman <ankit.dhiman@vinnisoft.org>
2024-09-24 12:58:42 +0530

    accordion lable

--- resources/views/components/accordion-label.blade.php
+++ resources/views/components/accordion-label.blade.php
@@ -3,8 +3,8 @@
         <input type="checkbox" name="id[]" value="{{ @$id }}" class="accordionCheckbox checkbox" data-name="{{ @$dataName }}">&nbsp;&nbsp;
         {{-- <input type="checkbox" value="{{ @$id }}" class="accordionCheckbox check-{{ $id }}" data-class="check-{{ $id }}">&nbsp;&nbsp; --}}
     </div>
-    <div class="col-8">{{ \Str::limit($name, 10, '...') ?? '-' }}</div>
-    <div class="col-2 d-flex">
+    <div class="col-{{ (@$edit && @$show) ? '7' : '8' }}">{{ \Str::limit($name, 20, '...') ?? '-' }}</div>
+    <div class="col-{{ (@$edit && @$show) ? '3' : '2' }} d-flex">
         @isset($edit)
             <a href="{{ $edit }}" class="me-3"><i class="bx bx-edit icon"></i></a>
         @endisset
