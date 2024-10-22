a8008761f09779e79ac905796245fad373e47da6
ankitdhiman <ankit.dhiman@vinnisoft.org>
2024-10-22 16:15:12 +0530

    Changed 419 msg

--- resources/views/errors/419.blade.php
+++ resources/views/errors/419.blade.php
@@ -4,6 +4,6 @@
         $lang = \App\Models\Setting::where('key', 'lang')->pluck('value')->first();
     @endphp
     <h1>{{ $lang == 'hb' ? 'פג תוקף העמוד' : 'Page Expired' }}</h1>
-    <p class="text mt-4">{{ $lang == 'hb' ? 'אנא לחץ כאן כדי להמשיך' : 'Please click here to continue' }}</p>
+    <p class="text mt-4">{{ $lang == 'hb' ? 'אנא לחץ כאן כדי להתחבר' : 'Please click here to login' }}</p>
     <a class="btn" style="background: #ffd681;" href="{{ url()->previous() }}">{{ $lang == 'hb' ? 'לְהַמשִׁיך' : 'Continue' }}</a>
 @endsection
