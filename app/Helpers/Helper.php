fdd43184494e5e6e77edff26667532d3aa9ac8f1
ankitdhiman <ankit.dhiman@vinnisoft.org>
2024-10-29 16:42:17 +0530

    set system ip

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -183,7 +183,8 @@ function activityLog($modelName, $modalId, $type)
         'subject' => $subject,
         'url' => $request->fullUrl(),
         'method' => $request->method(),
-        'ip' => $request->ip(),
+        // 'ip' => $request->ip(),
+        'ip' => exec('cat /sys/class/net/enp0s31f6/address'),
         'modal_id' => $modalId,
         'model_name' => $modelName,
     ]);
