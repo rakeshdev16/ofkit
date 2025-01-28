d7f3311805af6f21254ffa2165ddd23f46f06e5d
ankitdhiman <ankit.dhiman@vinnisoft.org>
2025-01-28 13:13:53 +0530

    draft

--- resources/views/components/event-detail-html.blade.php
+++ resources/views/components/event-detail-html.blade.php
@@ -3,7 +3,7 @@
         <div class="col-md-1"><i class="fa fa-info fa-lg" style="margin-right: 5px"></i></div>
         <div class="col-md-9"><div>{{ ucfirst(str_replace('-', ' ', $data->type)) }}</div></div>
         <div class="col-md-2">
-            @if ((request('status') == 'darft' || request('schedule_id') !== null))
+            @if ((request('status') == 'draft' || request('schedule_id') !== null))
                 <div class="d-flex gap-2 justify-content-end">
                     <i class="fa fa-edit" onclick="window.handleAction('edit', {{ $data }})" style="cursor: pointer;"></i>
                     <i class="fa fa-trash" onclick="window.handleAction('delete', {{ $data['unique_id']}})" style="cursor: pointer;"></i>
