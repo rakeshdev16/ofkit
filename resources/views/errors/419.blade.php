5d29870958d7a722f3fc5fe7a209dc40feca9415
ankitdhiman <ankit.dhiman@vinnisoft.org>
2025-02-03 16:48:34 +0530

    Fixed page expired issue

--- resources/views/errors/419.blade.php
+++ resources/views/errors/419.blade.php
@@ -2,8 +2,16 @@
 @section('section')
     @php
         $lang = \App\Models\Setting::where('key', 'lang')->pluck('value')->first();
+        switch (Route::currentRouteName()) {
+            case 'schedule.calendar':
+                $route = route('schedule.index');
+            break;
+            default:
+                $route = url()->previous();
+            break;
+        }
     @endphp
     <h1>{{ $lang == 'hb' ? 'פג תוקף העמוד' : 'Page Expired' }}</h1>
     <p class="text mt-4">{{ $lang == 'hb' ? 'אנא לחץ כאן כדי להתחבר' : 'Please click here to login' }}</p>
-    <a class="btn" style="background: #ffd681;" href="{{ url()->previous() }}">{{ $lang == 'hb' ? 'לְהַמשִׁיך' : 'Continue' }}</a>
+    <a class="btn" style="background: #ffd681;" href="{{ $route }}">{{ $lang == 'hb' ? 'לְהַמשִׁיך' : 'Continue' }}</a>
 @endsection
