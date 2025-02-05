bcc64379257472bf1aa05fc427e2fa243d8c4c4f
ankitdhiman <ankit.dhiman@vinnisoft.org>
2025-02-05 12:11:59 +0530

    status filter

--- resources/views/components/schedule-filter.blade.php
+++ resources/views/components/schedule-filter.blade.php
@@ -8,13 +8,15 @@
     'options' => $kindergartens,
     'disabled' => request('edit') == true ? 'disabled' : '',
 ])
-@include('components.select-input', [
-    'name' => '',
-    'icon' => 'buildings',
-    'value' => request('status'),
-    'onchange' => "filterCalendar({ 'status': this.value })",
-    'options' => [['key' => 'published', 'value' => 'Published'], ['key' => 'draft', 'value' => 'Saved as Draft']],
-])
+@if (Route::currentRouteName() == 'schedule.index')
+    @include('components.select-input', [
+        'name' => '',
+        'icon' => 'buildings',
+        'value' => request('status'),
+        'onchange' => "filterCalendar({ 'status': this.value })",
+        'options' => [['key' => 'published', 'value' => 'Published'], ['key' => 'draft', 'value' => 'Saved as Draft']],
+    ])
+@endif
 <span style="border: 2px solid #80808094"></span>
 <div id="childrenFilter"></div>
 <div id="staffFilter"></div>
