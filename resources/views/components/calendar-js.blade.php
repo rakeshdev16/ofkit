5532badf3923fc9443f7970b061e6a28a9733701
ankitdhiman <ankit.dhiman@vinnisoft.org>
2025-02-11 14:44:27 +0530

    col width

--- resources/views/components/calendar-js.blade.php
+++ resources/views/components/calendar-js.blade.php
@@ -55,7 +55,7 @@
         let windowWidth = window.screen.width;
         let columsCount = list.map((item) => (Array.isArray(item.children) ? item.children.length : 1)).reduce((sum, count) => sum + count, 0);
         let columnWidth = 100;
-        if (columsCount == 0) {
+        if (columsCount == 0 || columsCount <= 7) {
             columnWidth = (windowWidth-100)/7;
         } else if (columsCount*100 < windowWidth) {
             columnWidth = (windowWidth-100)/columsCount;
