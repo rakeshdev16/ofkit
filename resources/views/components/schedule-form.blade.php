10f32eb47adb32d9ceb7578d67b0895ae37bf22c
ankitdhiman <ankit.dhiman@vinnisoft.org>
2025-02-12 12:16:27 +0530

    Show selected time

--- resources/views/components/schedule-form.blade.php
+++ resources/views/components/schedule-form.blade.php
@@ -25,10 +25,10 @@
         </select>
     </div>
     <div class="w-50">
-        @include('components.time-picker', ['name' => 'start_time', 'label' => 'Start time'])
+        @include('components.time-picker', ['name' => 'start_time', 'label' => 'Start time', 'value' => @$data['startTime'].':00'])
     </div>
     <div class="w-50">
-        @include('components.time-picker', ['name' => 'end_time', 'label' => 'End time'])
+        @include('components.time-picker', ['name' => 'end_time', 'label' => 'End time', 'value' => @$data['endTime'].':00'])
     </div>
 </div>
 <div class="mb-3">
