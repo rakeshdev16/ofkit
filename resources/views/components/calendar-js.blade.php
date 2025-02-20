fca91af165dbbf048f2f2435aad34d8c9b3f1904
ankitdhiman <ankit.dhiman@vinnisoft.org>
2025-02-20 09:48:59 +0530

    fixed undefined issue

--- resources/views/components/calendar-js.blade.php
+++ resources/views/components/calendar-js.blade.php
@@ -108,16 +108,18 @@
                 args.header.html = hour.toString("HH:mm");
             },
             onTimeRangeSelected: async args => {
-                let isAvailableTime = availableTime.find(e => e.resource === args.resource);
-                if (isAvailableTime) {
-                    let availableStart = isAvailableTime.startTime.substring(0, 5);
-                    let availableEnd = isAvailableTime.endEnd.substring(0, 5);
-                    let selectedStart = args.start.toString("HH:mm");
-                    let selectedEnd = args.end.toString("HH:mm");
-                    if (selectedStart >= availableStart && selectedEnd <= availableEnd) {
-                        isTimeOutSide = false;
-                    } else {
-                        isTimeOutSide = true;
+                if (Array.isArray(availableTime) && availableTime.length > 0) {
+                    let isAvailableTime = availableTime.find(e => e.resource === args.resource);
+                    if (isAvailableTime) {
+                        let availableStart = isAvailableTime.startTime.substring(0, 5);
+                        let availableEnd = isAvailableTime.endEnd.substring(0, 5);
+                        let selectedStart = args.start.toString("HH:mm");
+                        let selectedEnd = args.end.toString("HH:mm");
+                        if (selectedStart >= availableStart && selectedEnd <= availableEnd) {
+                            isTimeOutSide = false;
+                        } else {
+                            isTimeOutSide = true;
+                        }
                     }
                 }
 
